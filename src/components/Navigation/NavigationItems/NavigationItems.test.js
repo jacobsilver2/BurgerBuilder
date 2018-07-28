b133e87a19944efa7bec85e040bc9fe983b4c22e
jacobsilver2 <jacobsilver2@mac.com>
2018-07-28 05:55:37 -0400

    more tests for navigationitems

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -11,10 +11,34 @@ import NavigationItem from './NavigationItem/NavigationItem'
 configure({adapter: new Adapter()});
 
 describe('<NavigationItems />', () => {
+
+  //! beforeEach allows things to happen before each test is run
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />)
+  });
+
   it('should render two <NavigationItem /> elements if not authenticated.', () => {
-    const wrapper = shallow(<NavigationItems />);
     //! this is not a JSX Element in the find function.  It's the normal export in the NavigationItem file.
     //! toHaveLength will expect to find 2 instances of NavigationItem
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
+  });
+
+  it('should render three <NavigationItem /> elements if authenticated.', () => {
+    //! using the setProps helper method
+    wrapper.setProps({
+      isAuthenticated: true
+    })
+    //! this is not a JSX Element in the find function.  It's the normal export in the NavigationItem file.
+    //! toHaveLength will expect to find 3 instances of NavigationItem
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
+
+  it('should render three <NavigationItem /> elements if authenticated.', () => {
+    wrapper.setProps({
+      isAuthenticated: true
+    })
+    //! look and see if there is this exact version of NavigationItem.  toEqual(true) expects to find this if we are authenticated
+    expect(wrapper.contains(<NavigationItem link="/logout">LOGOUT</NavigationItem>)).toEqual(true);
   });
 })
